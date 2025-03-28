cf76cfebc6e005567aa59bf530482613c168c3cb
Joao Eduardo Beraldo Iada <joaoed.beraldo@gmail.com>
2025-03-27 21:37:41 -0400

    add dev info page

--- mobile-app/app/src/main/java/com/example/sequencememorygame/MainActivity.kt
+++ mobile-app/app/src/main/java/com/example/sequencememorygame/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.sequencememorygame
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nameInput = findViewById<EditText>(R.id.edtUserName)
+
+//      Buttons below
+        val instructionsButton = findViewById<Button>(R.id.btnInstructions)
+        val playButton = findViewById<Button>(R.id.btnPlay)
+        val rankingButton = findViewById<Button>(R.id.btnHighScore)
+        val devButton = findViewById<Button>(R.id.btnDev)
+
+        instructionsButton.setOnClickListener {
+            val intent = Intent(this, InstructionsActivity::class.java)
+            startActivity(intent)
+        }
+        devButton.setOnClickListener {
+            val intent = Intent(this, DevActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
